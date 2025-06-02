27cbb61a6013d15148743624ddbd706611b0679f
camilag <c.guayara@uniandes.edu.co>
2025-06-02 03:49:52 -0500

    se habilitan cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from starlette.middleware.cors import CORSMiddleware
+
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 from fastapi import FastAPI
@@ -8,6 +10,13 @@ from app.api.v1.routers import auth, role
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  #nota cambiar cuando este en desarrollo para recibir request de un solo front
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
